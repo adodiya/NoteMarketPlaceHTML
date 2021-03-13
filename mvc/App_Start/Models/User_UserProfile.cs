53a72acfdc5781f42ae79d78beadc4b15d93725c
adodiya <77188018+adodiya@users.noreply.github.com>
2021-03-13 10:45:06 +0530

    Add files via upload

---
+++ mvc/App_Start/Models/User_UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace NotesMarketPlace.Models
+{
+    public class User_UserProfile
+    {
+        public User User { get; set; }
+        public UserProfile UserProfile { get; set; }
+
+        public Country Country { get; set; }
+    }
+}
