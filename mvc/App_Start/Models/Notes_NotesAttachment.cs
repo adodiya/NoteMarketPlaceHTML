927c4dea22671ce162bbec61e63fa6fb9fdc6803
adodiya <77188018+adodiya@users.noreply.github.com>
2021-03-27 23:51:21 +0530

    Add files via upload

---
+++ mvc/App_Start/Models/Notes_NotesAttachment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotesMarketPlace.Models
+{
+    public class Notes_NotesAttachment
+    {
+
+        public Note Note { get; set; }
+        
+        public NotesAttachment  NotesAttachment { get; set; }
+
+       public List<NotesAttachment> AttachmentList { get; set; }
+    }
+}
